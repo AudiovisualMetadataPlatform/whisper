574d18766b16d185e0bbec0b49c0bc9f8a1a6159
Brian Wheeler <bdwheele@iu.edu>
2023-06-08 13:05:36 -0400

    working driver script

--- whisper.py
+++ whisper.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # Driver for whisper.sif
-import os.path
 import shutil
 import subprocess
 import sys
@@ -8,10 +7,8 @@ import tempfile
 import argparse
 import logging
 from pathlib import Path
-import math
-import atexit
-import os
-import time
+import json
+
 
 whisper_languages = [
     'Auto', 'Afrikaans', 'Albanian', 'Amharic', 'Arabic', 'Armenian', 'Assamese', 'Azerbaijani',
@@ -63,71 +60,78 @@ def main():
         logging.error("You must select an output of some sort!")
         exit(1)
 
+    sif = Path(sys.path[0], "whisper.sif")
+    if not sif.exists():
+        logging.error(f"Whisper SIF file {sif!s} not found")
+        exit(1)
 
-    # copy the input file to a temporary directory
-    with tempfile.TemporaryDirectory() as tmpdir:        
-        shutil.copy(args.input_audio, f"{tmpdir}/xxx.wav")
-        # find the right Kaldi SIF and set up things to make it work...
-        sif = Path(sys.path[0], f"kaldi-pua-{'gpu' if args.gpu else 'cpu'}.sif")
-        if not sif.exists():
-            logging.error(f"Kaldi SIF file {sif!s} doesn't exist!")
-            exit(1)
-
-        # By default, singularity will map $HOME, /var/tmp and /tmp to somewhere outside
-        # the container.  That's good.  
-        # The authors of the kaldi docker image assumed that they could write anywhere
-        # they pleased on the container image.  That's bad.
-        # With the --writable-tmpfs, singularity will produce a 16M overlay filesystem that
-        # handles writes everywhere else.  That's good.
-        # BUT kaldi writes big files all over the place...and they will routinely exceed
-        # 16M.  That's bad.  
-        # The bottom line is that we have to create an overlay image that's big enough
-        # for what we're trying to do.  But how big?  No matter what size we use, it will
-        # never be enough for some cases.  For now, let's look at the size of the input file
-        # (which should be a high-bitrate wav) and use some multiple.  Empirically, it looks
-        # like 10x should do the trick. 
-        overlay_size = math.ceil((10 * Path(args.input_audio).stat().st_size) / 1048576)
-        if overlay_size < 64:
-            overlay_size = 64
-        if args.overlay_dir is None:
-            args.overlay_dir = str(Path('.').absolute())
-        else:
-            args.overlay_dir = str(Path(args.overlay_dir[0]).absolute())
-        overlay_file = f"{args.overlay_dir}/kaldi-overlay-{os.getpid()}-{time.time()}.img"
-        
-        if not args.debug:
-            # make sure to erase the overlay at the end.  This is kind of an abuse of lambda...
-            atexit.register(lambda: Path(overlay_file).unlink() if Path(overlay_file).exists() else None)
+    with tempfile.TemporaryDirectory(prefix="whisper-") as tmpdir:
+        logging.debug(f"Temporary directory: {tmpdir}")
+        whisper_args = ["--model_dir", whisper_model_dir,
+                        "--output_dir", tmpdir,
+                        "--model", args.model,
+                        "--word_timestamps", "True",
+                        args.input_media]
+        if args.language != "Auto":
+            whisper_args.extend(['--language', args.language])
         try:
-            subprocess.run(["singularity", "overlay", "create", "--size", str(overlay_size), overlay_file], check=True)
-            logging.debug(f"Created overlay file {overlay_file} {overlay_size}MB")
-        except subprocess.CalledProcessError as e:
-            logging.exception(f"Cannot create the overlay image of {overlay_size} bytes as {overlay_file}!")            
+            subprocess.run([str(sif), *whisper_args], check=True)
+        except Exception as e:
+            logging.exception(f"Failed to transcribe: {e}")
+            exit(1)
+                        
+        try:
+            # go through each of the requested outputs and gather them.
+            if args.transcript_json:
+                shutil.copy(get_file_by_ext(tmpdir, 'json'), args.transcript_json)
+
+            if args.transcript_text:
+                shutil.copy(get_file_by_ext(tmpdir, 'txt'), args.transcript_text)
+
+            if args.webvtt:
+                shutil.copy(get_file_by_ext(tmpdir, 'vtt'), args.webvtt)
+
+            if args.amp_transcript:
+                # convert the native json transcript to an amp transcript
+                with open(get_file_by_ext(tmpdir, 'json')) as f:
+                    data = json.load(f)
+                amp_transcript = {
+                    'media': {
+                        'filename': args.input_media,
+                        'duration': 0
+                    },
+                    'results': {
+                        'transcript': data['text'],
+                        'words': []
+                    }
+                }                
+                duration = 0
+                for seg in data['segments']:
+                    for word in seg['words']:
+                        amp_transcript['results']['words'].append({
+                            'type': "",
+                            'text': word['word'],
+                            'start': word['start'],
+                            'end': word['end']
+                        })
+                        duration = max(duration, word['end'])
+                
+                amp_transcript['results']['duration'] = duration 
+
+                with open(args.amp_transcript, "w") as f:
+                    json.dump(amp_transcript, f)
+
+        except Exception as e:
+            logging.exception(f"Failed to gather outputs: {e}")
             exit(1)
 
-        # build the singularity command line
-        cmd = ['singularity', 'run', '-B', f"{tmpdir}:/audio_in", '--overlay', overlay_file, str(sif) ]
-        logging.debug(f"Singularity Command: {cmd}")
-        p = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding='utf-8')        
-        if p.returncode != 0:
-            logging.error("KALDI failed")
-            logging.error(p.stdout)
-            exit(1)
-        copy_failed = False
-        for src, dst in ((f"{tmpdir}/transcripts/txt/xxx_16kHz.txt", args.kaldi_transcript_text),
-                         (f"{tmpdir}/transcripts/json/xxx_16kHz.json", args.kaldi_transcript_json)):
-            try:                
-                shutil.copy(src, dst)
-            except Exception as e:
-                logging.exception(f'Cannot copy {src} to {dst}')     
-                copy_failed = True
-        if copy_failed:
-            logging.error("Kaldi didn't actually produce the files on a 'successful' run")
-            logging.error(f"Kaldi's output:\n{p.stdout}")
 
-    logging.debug(f"Make sure to manually remove the overlay file: {overlay_file}")
-    logging.info(f"Finished running Kaldi with output {args.kaldi_transcript_json} and {args.kaldi_transcript_text}")
-    exit(0 if not copy_failed else 1)
+def get_file_by_ext(path, ext):
+    files = list(Path(path).glob(f"*.{ext}"))
+    if files:
+        return files[0]
+    else:
+        raise FileNotFoundError(f"Cannot find file with extension {ext} in {path}")
 
 if __name__ == "__main__":
     main()
